953e5879cec63a7873d4580dd5fdd402e146ee8e
WatchJani <jankokondic84@gmail.com>
2024-08-30 18:10:34 +0200

    stack

---
+++ main.go
@@ -0,0 +1,48 @@
+package stack
+
+import (
+	"fmt"
+)
+
+const ErrEmpty = "stack is empty"
+
+type Stack[T any] struct {
+	store []T
+}
+
+func NewStack[T any](capacity int) Stack[T] {
+	return Stack[T]{
+		store: make([]T, 0, capacity),
+	}
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.store = append(s.store, value)
+}
+
+func (s *Stack[T]) Clear() {
+	s.store = s.store[:0]
+}
+
+func (s *Stack[T]) Pop() (T, error) {
+	if len(s.store) == 0 {
+		var zeroValue T
+		return zeroValue, fmt.Errorf("%s", ErrEmpty)
+	}
+	value := s.store[len(s.store)-1]
+	s.store = s.store[:len(s.store)-1]
+	return value, nil
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return len(s.store) == 0
+}
+
+func (s *Stack[T]) Peek() (T, error) {
+	if len(s.store) == 0 {
+		var zeroValue T
+		return zeroValue, fmt.Errorf("%s", ErrEmpty)
+	}
+
+	return s.store[len(s.store)-1], nil
+}
---
+++ main_test.go
@@ -0,0 +1,18 @@
+package stack
+
+import "testing"
+
+const TEST_INPUT = 1000
+
+func BenchmarkCustom(b *testing.B) {
+	b.StopTimer()
+	stack := NewStack[int](TEST_INPUT)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		for index := range TEST_INPUT {
+			stack.Push(index)
+		}
+
+		stack.Clear()
+	}
+}
